a4b7939dd2a0a218bb4a16b8ba3971f46dd8f26c
Michael Herrmann <michael.herrmann@omaha-consulting.com>
2020-07-19 16:35:16 +0200

    Add better dev environment setup instructions

---
+++ omaha_consulting/env.cmd
@@ -0,0 +1,14 @@
+set WIX=C:\Program Files (x86)\WiX Toolset v3.11
+rem For building with Visual Studio 2017:
+set OMAHA_MSC_VER=1910
+set OMAHA_SIGNTOOL_SDK_DIR=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64
+set WindowsSdkDir=C:\Program Files (x86)\Windows Kits\10\
+set WindowsSDKVersion=10.0.18362.0\
+set WindowsSdkVerBinPath=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0
+set OMAHA_PSEXEC_DIR=C:\Program Files (x86)\pstools
+
+set TIMESTAMP_SERVER=http://timestamp.digicert.com
+set SHA1_TIMESTAMP_SERVER=http://timestamp.digicert.com
+set SHA2_TIMESTAMP_SERVER=http://timestamp.digicert.com
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
