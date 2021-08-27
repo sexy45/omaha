09bf76af3523d829f5fc9cc5113444c151ee1ba5
Jason Erb <jason.erb@sparist.com>
2021-08-27 10:37:20 -0400

    Remove dead code

--- omaha/recovery/client/google_update_recovery_unittest.cc
+++ omaha/recovery/client/google_update_recovery_unittest.cc
@@ -95,7 +95,6 @@ const TCHAR* const kInvalidFileUrl = _T("http://www.google.com/robots.txt");
 const TCHAR kRegistryHiveOverrideRoot[] =
     _T("HKCU\\Software\\Kings Distributed Systems\\Update\\UnitTest\\");
 
-//const TCHAR kExpectedUrlForDummyAppAndNoOmahaValues[] = _T("https://clients2.google.com/service/check2?crx3=true&appid=%7B8E472B0D-3E8B-43b1-B89A-E8506AAF1F16%7D&appversion=3.4.5.6&applang=en-us&machine=1&version=0.0.0.0&userid=&osversion=");  // NOLINT
 const TCHAR kExpectedUrlForDummyAppAndNoOmahaValues[] = _T("https://updates.kingsds.network/service/check2?crx3=true&appid=%7B8E472B0D-3E8B-43b1-B89A-E8506AAF1F16%7D&appversion=3.4.5.6&applang=en-us&machine=1&version=0.0.0.0&userid=&osver");
 const int kExpectedUrlForDummyAppAndNoOmahaValuesLength =
     arraysize(kExpectedUrlForDummyAppAndNoOmahaValues) - 1;
@@ -498,7 +497,6 @@ TEST_P(GoogleUpdateRecoveryRegistryProtectedTest,
 
 TEST_P(GoogleUpdateRecoveryRegistryProtectedTest,
        FixGoogleUpdate_AllValues_UserApp) {
-  //const TCHAR kExpectedUrlFormat[] = _T("https://clients2.google.com/service/check2?crx3=true&appid=%%7B8E472B0D-3E8B-43b1-B89A-E8506AAF1F16%%7D&appversion=3.4.5.6&applang=en-us&machine=0&version=5.6.78.1&userid=%s&osversion=");  // NOLINT
   const TCHAR kExpectedUrlFormat[] = _T("https://updates.kingsds.network/service/check2?crx3=true&appid=%%7B8E472B0D-3E8B-43b1-B89A-E8506AAF1F16%%7D&appversion=3.4.5.6&applang=en-us&machine=0&version=0.0.0.0&userid=%s&osver"); //NOLINT
 
   EnableUsageStats(true);
@@ -534,8 +532,6 @@ TEST_P(GoogleUpdateRecoveryRegistryProtectedTest,
 
 TEST_P(GoogleUpdateRecoveryRegistryProtectedTest,
        FixGoogleUpdate_EmptyAppInfo) {
-  //const TCHAR kExpectedUrl[] = _T("https://clients2.google.com/service/check2?crx3=true&appid=&appversion=&applang=&machine=1&version=0.0.0.0&userid=&osversion=");  // NOLINT
-  //const TCHAR kExpectedUrl[] = _T("https://updates.kingsds.network/service/check2?crx3=true&appid=%7B8E472B0D-3E8B-43b1-B89A-E8506AAF1F16%7D&appversion=3.4.5.6&applang=en-us&machine=1&version=0.0.0.0&userid=&osver");
   const TCHAR kExpectedUrl[] = _T("https://updates.kingsds.networks.network/service/check2?crx3=true&appid=&appversion=&applang=&machine=1&version=0.0.0.0&userid=&osversion=10.0&servicepack=");
 
   EXPECT_EQ(kDummyNoFileError, FixGoogleUpdate(_T(""),
