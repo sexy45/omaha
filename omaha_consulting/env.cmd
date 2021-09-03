437fa965045cf58761dc2b207c2c7b32b5c39f5c
Jason Erb <jason.erb@sparist.com>
2021-09-03 15:49:51 -0400

    Refactor env.cmd to deduplicate

--- omaha_consulting/env.cmd
+++ omaha_consulting/env.cmd
@@ -1,15 +1,15 @@
-set WIX=C:\Program Files (x86)\WiX Toolset v3.11
-set OMAHA_SIGNTOOL_SDK_DIR=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64
-set WindowsSdkDir=C:\Program Files (x86)\Windows Kits\10\
+set WIX=%programfiles(x86)%\WiX Toolset v3.11
 set WindowsSDKVersion=10.0.18362.0\
-set WindowsSdkVerBinPath=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0
-set OMAHA_PSEXEC_DIR=C:\Program Files (x86)\pstools
+set WindowsSdkDir=%programfiles(x86)%\Windows Kits\10\
+set OMAHA_SIGNTOOL_SDK_DIR=%WindowsSdkDir%\bin\%WindowsSDKVersion%\x64
+set WindowsSdkVerBinPath=%WindowsSdkDir%\bin\%WindowsSDKVersion%
+set OMAHA_PSEXEC_DIR=%programfiles(x86)%\pstools
 
 set TIMESTAMP_SERVER=http://timestamp.digicert.com
 set SHA1_TIMESTAMP_SERVER=http://timestamp.digicert.com
 set SHA2_TIMESTAMP_SERVER=http://timestamp.digicert.com
 
 rem Change "Community" to "Professional", etc., if using a non-community version of VS.
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 
 rem call C:\Python27\python\Scripts\activate.bat
